0aa89c88b8a3019a11bb5a0e23fe2494c2cb27b2
Feyenoord515 <n.enriquemolinari@gmail.com>
2023-03-02 20:16:10 -0300

    se agrego color y estilo con tw

--- melatube/src/App.js
+++ melatube/src/App.js
@@ -34,28 +34,10 @@ const App = () => {
   }, []);
 
   return (
-    <div>
+    <div className="bg-pink-200">
       <Navbar />
     <div className="container mx-auto p-4">
-    <ChannelList channels={channels} />
-      {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-      {channels.map((channel) => (
-        <div key={channel.id.channelId} className="border border-gray-300 p-4 rounded">
-      <a href={`https://www.youtube.com/channel/${channel.id.channelId}`} target="_blank" rel="noopener noreferrer">
-        <img
-          src={channel.snippet.thumbnails.medium.url}
-          alt={channel.snippet.title}
-          className="w-full h-48 object-cover rounded mb-2"
-          />
-        <h2 className="text-xl font-bold mb-2">{channel.snippet.title}</h2>
-        <p className="text-gray-700">{channel.snippet.description}</p>
-      </a>
-    </div>
-  ))}
-   </div>
-    */}
-   
-    
+    <ChannelList channels={channels} />   
   </div>
     <div> 
        
--- melatube/src/components/Navbar.js
+++ melatube/src/components/Navbar.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav className="bg-pink-500  border-blue-700 px-4 py-2 flex justify-between items-center">
-    <div>
-        <a href="#" className="text-white font-bold text-xl">Canales para niños en YouTube</a>
-      </div>
+    <nav className="bg-blue-200  border-blue-700 px-4 py-2 flex justify-between items-center">
+   <div className="flex items-center justify-between bg-green-500 p-4">
+  <a href="#" className="text-white font-bold text-2xl">
+    <img src="https://cdn-icons-png.flaticon.com/512/1384/1384031.png" alt="Logo" className="w-10 h-10 mr-2" />
+    Canales para niños en YouTube
+  </a>
+</div>
       
       <ul className="flex justify-end space-x-4">
         <li>
